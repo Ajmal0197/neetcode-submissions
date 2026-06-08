2a1e07feed4906756fa713c90efd4bf0c36e67a3
Ajmal Hasan <ajmal0197@gmail.com>
2026-06-08 13:49:48 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        let map = new Map();
+
+        // Count characters from s
+        for (let char of s) {
+            map.set(char, (map.get(char) || 0) + 1);
+        }
+
+        // Remove counts using t
+        for (let char of t) {
+
+            if (!map.has(char)) {
+                return false;
+            }
+
+            map.set(char, map.get(char) - 1);
+
+            if (map.get(char) === 0) {
+                map.delete(char);
+            }
+        }
+
+        return map.size === 0;
+    }
+}
