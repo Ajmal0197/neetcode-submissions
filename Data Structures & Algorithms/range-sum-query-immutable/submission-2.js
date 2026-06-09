cea2299ab7ae51eb61599d0318ebdbd5e88ef923
Ajmal Hasan <ajmal0197@gmail.com>
2026-06-10 01:15:38 +0530

    Add: range-sum-query-immutable - submission-2

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-2.js
@@ -0,0 +1,15 @@
+class NumArray {
+    constructor(nums) {
+        let curr = 0
+        this.prefix = [0]
+        for(let num of nums){
+            curr += num
+            this.prefix.push(curr)
+        }
+    }
+
+    sumRange(left, right) {
+        let res = this.prefix[right+1] - this.prefix[left]
+        return res
+    }
+}
