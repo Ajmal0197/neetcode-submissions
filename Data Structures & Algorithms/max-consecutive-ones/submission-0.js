771e8827a104cee047fbee497a60deda6693558f
Ajmal Hasan <ajmal0197@gmail.com>
2026-06-13 17:51:00 +0530

    Add: max-consecutive-ones - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums) {
+            let count = 0;
+    let max = 0;
+
+    for(let num of nums) {
+
+        if(num === 1) {
+            count++;
+            max = Math.max(max, count);
+        } else {
+            count = 0;
+        }
+    }
+
+    return max;
+    }
+}
